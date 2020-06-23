0d75eb8c3e7d97234cbd360e7de41f1fa97cf3ba
Ricardo <manosalvas2013@gmail.com>
2020-06-23 18:49:33 -0400

    framework

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+var movie = "Mr. Nobody";
+var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+
+});
