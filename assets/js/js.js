fbe94a3977600825b97ffad9be92cf6335f9f9ae
sidorela Kollcaku <sidorelakollcaku@gmail.com>
2020-06-23 21:40:13 -0400

    modified

--- assets/js/js.js
+++ assets/js/js.js
@@ -11,16 +11,30 @@ $.ajax({
 
         });
 
+var yelpKey = "mPsfs7mhH6BODbu6jrYiS1QSDo0s8lxpNkdPVTet2qp0AHaJ61lp2oyqJ_2MRiJaCBqEIebiD8tCpIxAYE5hU_4ZXJqlYU1eQPmOkfPDADRkp9yXyy8VOWSOvabyXnYx";
 
-var rickKey = "AIzaSyDoNHm29IP6HZeVrcltuCkZ1AMzrhnNZTs"
-var queryURLx = "https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters/?" + "key=" + rickKey;
+var queryURL1 = "https://api.yelp.com/v3/businesses/search?location=NewYorkCity?" + yelpKey;  
 
 $.ajax({
+    url: queryURL1,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
 
-            url: queryURLx,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
+});
 
-        });
+
+
+
+// var rickKey = "AIzaSyDoNHm29IP6HZeVrcltuCkZ1AMzrhnNZTs"
+// var queryURLx = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Sydney&key=" + rickKey;
+
+// $.ajax({
+
+//             url: queryURLx,
+//             method: "GET"
+//         }).then(function (response) {
+//             console.log(response);
+
+//         });
 
