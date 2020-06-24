040518b7eb6173d3b04a0ad67947a3486f0e57fe
Ricardo <manosalvas2013@gmail.com>
2020-06-23 20:28:23 -0400

    nothing

--- assets/js/js.js
+++
@@ -1,9 +0,0 @@
-var movie = "Mr. Nobody";
-var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-
-});
