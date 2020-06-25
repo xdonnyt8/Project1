b200a378a8c0260d1cb421eaa981676eaa712846
Don Tran <mr.dontran@gmail.com>
2020-06-24 21:11:23 -0400

    added bulma css and created a few containers and sections

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,9 +1,61 @@
-var movie = "Mr. Nobody";
-var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+var url = "https://wger.de/api/v2/muscle/";
+url += '?' + $.param({
+ 'api-key': "51698d92c596b039779271966d708d4f1ba07b56"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+  dataType: 'JSON',
+  success: function(data) {
+    console.log(data)
+  },
+  error: function(err) {
+    console.log('error:' + err)
+  }
+})
+
+var endpoint = "";
+// var queryURL = "https://wger.de/api/v2/" + endpoint + "/" +  key;
+var queryURL = "https://wger.de/api/v2/" + "?217452dcb87aabe91d2071261915796cb41761c2007b3444879798f58c7";
+
 
 $.ajax({
-    url: queryURL,
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+        });
+
+
+
+
+var yelpKey = "mPsfs7mhH6BODbu6jrYiS1QSDo0s8lxpNkdPVTet2qp0AHaJ61lp2oyqJ_2MRiJaCBqEIebiD8tCpIxAYE5hU_4ZXJqlYU1eQPmOkfPDADRkp9yXyy8VOWSOvabyXnYx";
+
+var queryURL1 = "https://api.yelp.com/v3/businesses/search?location=New York City/?" + yelpKey;  
+ 
+$.ajax({
+    url: queryURL1,
     method: "GET"
 }).then(function (response) {
+    console.log(response);
 
 });
+
+
+
+
+
+
+
+// var rickKey = "AIzaSyCfMXFMDU8B8So2xUI2efm3iPtiikMiNYU"
+// var queryURLx = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Sydney&key=" + rickKey;
+
+// $.ajax({
+
+//             url: queryURLx,
+//             method: "GET"
+//         }).then(function (response) {
+//             console.log(response);
+
+//         });
